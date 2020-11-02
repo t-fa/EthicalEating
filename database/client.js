632c67c64b0bf607d92fb35091c042b4fac33206
thomas <55923526+thomas-cs@users.noreply.github.com>
2020-11-01 20:47:59 -0800

    small tweaks for Heroku compatability

--- database/client.js
+++ database/client.js
@@ -15,12 +15,16 @@ const mysql = require("mysql2");
 // secrets out of the repository. We can create a top-level
 // configuration file to hold the createPool instantiation
 // object. For now for development and testing, this is fine.
+
+// When connecting to the MySQL database on Heroku, the @host, @user, @password,
+// and @database are provided to the app as environmental variables. Use these
+// variables if they are set. Otherwise, use the default values for testing.
 const pool = mysql.createPool({
-  host: "db",
+  host: process.env.DATABASE_HOST || "db",
   port: "3306",
-  user: "root",
-  password: "secret",
-  database: "EthicalEating",
+  user: process.env.DATABASE_USER || "root",
+  password: process.env.DATABASE_PASSWORD || "secret",
+  database: process.env.DATABASE_NAME || "EthicalEating",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
