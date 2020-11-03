a7f6142f369f76661b753b985029cda8418055b7
xHoudek <xhoudek@gmail.com>
2020-11-03 16:27:24 -0600

    'Start Over' button functionality

--- public/js/buildRecipe.js
+++ public/js/buildRecipe.js
@@ -1,5 +1,6 @@
 function buttonFunctions() {
     document.getElementById('addIngredient').addEventListener('click', addIngredients)
+    document.getElementById('startOver').addEventListener('click', startOver)
 }
 
 let ingredientCount = 1;
@@ -8,7 +9,7 @@ function addIngredients() {
     let ingredientFields = document.getElementById('ingredientFields')
     let ingredientSelect = document.createElement('div');
     let className = 'ingredient' + ingredientCount
-    ingredientSelect.className = className
+    ingredientSelect.className = 'added ' + className
     let htmlContent = document.getElementById('ingredientSelect').innerHTML
     htmlContent += '<button onclick="removeIngredient(\'' + className + '\');">Remove Ingredient</button>'
     ingredientSelect.innerHTML = htmlContent
@@ -16,7 +17,16 @@ function addIngredients() {
 }
 
 function removeIngredient(ingredient) {
-    document.getElementsByClassName(ingredient)[0].remove()
+    document.getElementsByClassName(ingredient)[0].remove();
+}
+
+function startOver() {
+    
+    let addedElements = document.getElementsByClassName('added');
+    console.log(addedElements);
+    for (var i = addedElements.length - 1; i >= 0 ; i--) {
+        addedElements[i].remove(); 
+    };
 }
 
 document.addEventListener("DOMContentLoaded", buttonFunctions)
