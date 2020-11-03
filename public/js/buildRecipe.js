d706a0bc26aeb443c2af9ad02c497492ed3f72cb
xHoudek <xhoudek@gmail.com>
2020-11-03 15:18:06 -0600

    "Add ingredient" button functionality

--- public/js/buildRecipe.js
+++ public/js/buildRecipe.js
@@ -1,4 +1,18 @@
-//I want this to be used in buildRecipe.handlebars
-let context = {
-    iterator: ['']
+function buttonFunctions() {
+    document.getElementById('addIngredient').addEventListener('click', addIngredients)
 }
+
+let ingredientCount = 1;
+function addIngredients() {
+    ingredientCount++;
+    var objTo = document.getElementById('ingredientFields')
+    var divtest = document.createElement('div');
+
+    var ingredientSelect = document.getElementById('ingredientSelect').innerHTML
+
+
+    divtest.innerHTML = ingredientSelect
+    objTo.appendChild(divtest)
+}
+
+document.addEventListener("DOMContentLoaded", buttonFunctions)
