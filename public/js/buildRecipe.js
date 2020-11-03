a26c08a6647b1795707ad7693559cf48d736cb19
xHoudek <xhoudek@gmail.com>
2020-11-03 16:44:17 -0600

    added temp icon, commented buildRecipe.js

--- public/js/buildRecipe.js
+++ public/js/buildRecipe.js
@@ -1,8 +1,10 @@
+// load button functionality
 function buttonFunctions() {
     document.getElementById('addIngredient').addEventListener('click', addIngredients)
     document.getElementById('startOver').addEventListener('click', startOver)
 }
 
+// adds a new div containing ingredient dropdown as well as option to remove element
 let ingredientCount = 1;
 function addIngredients() {
     ingredientCount++;
@@ -16,17 +18,18 @@ function addIngredients() {
     ingredientFields.appendChild(ingredientSelect)
 }
 
+// removes element that contains the button
 function removeIngredient(ingredient) {
     document.getElementsByClassName(ingredient)[0].remove();
 }
 
+// removes added elements, does not affect recipe name or first ingredient selection
 function startOver() {
-    
     let addedElements = document.getElementsByClassName('added');
-    console.log(addedElements);
     for (var i = addedElements.length - 1; i >= 0 ; i--) {
         addedElements[i].remove(); 
     };
 }
 
+// wait for page content to load
 document.addEventListener("DOMContentLoaded", buttonFunctions)
