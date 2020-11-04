ecfe3d8b30b4e40c23de74d70b889c1f7363d8cd
ADLS123 <aguedo.delossantos@gmail.com>
2020-11-04 09:12:18 -0800

    Added logic to search.handlbars to display list, and message when recipe not found. Updated javascript to fix error when no search term is entered

--- routes/search_router.js
+++ routes/search_router.js
@@ -13,9 +13,9 @@ search_recipe_router.route('/')
     context = {}
     // Search term that is passed to the database query
     var search_term = req.query.recipe_search;
-    console.log("Search Term Is", req.query)
+    console.log("Search Term Is", search_term)
     
-    
+	if (search_term != undefined)  {
     Models.Recipes.searchByName({"query":search_term},(err, Recipe_List) => {
         if (err) {
             console.log("Failed to fetch Ingredients:", err);
@@ -25,9 +25,12 @@ search_recipe_router.route('/')
           console.log("Recipes:", Recipe_List);
           console.log("Recipe_List JSON Object", Recipe_List.map(recipes => recipes.toJSON()));
           context.recipes = Recipe_List;
+          
           res.render('search', context);
+
           }
-    })
+    })}
+    else{res.render('search');}
 })
 
 module.exports = search_recipe_router;
