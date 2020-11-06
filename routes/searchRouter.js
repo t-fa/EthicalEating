9e29d8c39085573b0caf050430446c7c65770597
Tom Fattah <thomas.fattah@gmail.com>
2020-11-06 11:48:13 -0500

    Fixed endpoint to correctly return ingredients

--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -6,24 +6,12 @@ const searchRouter = express.Router();
 searchRouter.use(bodyParser.urlencoded({ extended: false }));
 searchRouter.use(bodyParser.json());
 
+/* Endpoint for searching for recipes. Returns a list of recipes matching a search term
+and associated ingredients accesible in context.recipes and context.ingredients, respectively.s
+*/
 searchRouter.route('/')
 .get((req, res) => {
-    if(req.query.ingredient){
-        context = {};
-        const search = req.query.ingredient_search;
-
-        Models.Ingredients.searchByName({"query": search}, (err, listOfIngredients) => {
-            if (err) {
-            console.log("Failed to fetch Ingredients:", err);
-            return next(err);  // bail out of the handler here, listOfIngredients undefined
-            }
-            // Got the listOfFlourIngredients.
-            console.log("listOfIngredients:", listOfIngredients);
-            console.log("listOfIngredients as json", listOfIngredients.map(ingredient => ingredient.toJSON()));
-            context.ingredients = listOfIngredients;
-            res.render('search', context);
-        });
-    } else if(req.query.recipe_search) {
+    if(req.query.recipe_search) {
         context = {};
         const search = req.query.recipe_search;
 
@@ -46,8 +34,8 @@ searchRouter.route('/')
                     // Got the Ingredients list.
                     console.log("listOfIngredients:", listOfIngredients);
                     console.log("listOfIngredients as json", listOfIngredients.map(ingredient => ingredient.toJSON()));
+                    context.ingredients = listOfIngredients
                   });
-                  context.ingredients = listOfIngredients
             })
             res.render('search', context);
           });
@@ -56,6 +44,30 @@ searchRouter.route('/')
     }
 });
 
+/* Endpoint for searching for ingredients. Returns a list of ingredients matching the search term
+accessible in context.ingredients.
+*/
+searchRouter.route('/ingredient_search')
+.get((req, res, next) => {
+    if(req.query.ingredient_search){
+        context = {};
+        const search = req.query.ingredient_search;
+
+        Models.Ingredients.searchByName({"query": search}, (err, listOfIngredients) => {
+            if (err) {
+            console.log("Failed to fetch Ingredients:", err);
+            return next(err);  // bail out of the handler here, listOfIngredients undefined
+            }
+            // Got the listOfIngredients.
+            console.log("listOfIngredients:", listOfIngredients);
+            console.log("listOfIngredients as json", listOfIngredients.map(ingredient => ingredient.toJSON()));
+            context.ingredients = listOfIngredients;
+            res.render('ingredient_search', context);
+        });
+    } else {
+        res.render('ingredient_search');
+    }
+})
 
 
 module.exports = searchRouter;
