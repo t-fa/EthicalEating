29e19114a6ce18d1a22bd644963983e482469ec4
ADLS123 <aguedo.delossantos@gmail.com>
2020-11-18 18:17:58 -0800

    Added functionality for displaying link to all ingredients dynamically

--- public/js/buildRecipe.js
+++ public/js/buildRecipe.js
@@ -17,21 +17,42 @@ function addIngredients() {
     let htmlContent = document.getElementById('ingredientSelect').innerHTML
     htmlContent += '<button onclick="removeIngredient(\'' + className + '\');">Remove Ingredient</button>'
     htmlContent += '<br><a href="/ingredientEthics/2"id ="recipe_link'+ ingredientCount+ '">Ingredient Detail</a>'
+    
+     ingredientSelect.innerHTML = htmlContent
+    ingredientFields.appendChild(ingredientSelect)
+	let htmlContent2= '<script> getVal'+ ingredientCount+'() function getVal'+ ingredientCount+'() { var selectedVal = document.getElementById("ingredients'+ ingredientCount+'").value; var link = document.getElementById("recipe_link"); var final = /ingredientEthics/ + selectedVal link.setAttribute("href", final);console.log(selectedVal);}</script>'
+	
+	//let htmlContent2= 'function getVal2(){console.log("made it in here atleast")}'
+   
+   
+    //document.getElementById('first').insertAdjacentHTML('beforeend', '<div id="idChild"> content html </div>');
+    var test = document.createElement("script");
+    test.innerHTML = htmlContent2;
+    
+    //document.getElementById("first").insertAdjacentHTML("afterend", test);
+    console.log('first is',document.getElementById('first'))
+    
+    console.log("yo:", '<script> getVal'+ ingredientCount+'() function getVal'+ ingredientCount+'() { var selectedVal = document.getElementById("ingredients'+ ingredientCount+'").value; var link = document.getElementById("recipe_link"); var final = /ingredientEthics/ + selectedVal link.setAttribute("href", final);console.log(selectedVal);}</script>')
     // Used for bug testing
     curr = '<br><a href="/ingredientEthics/2"id ="recipe_link'+ ingredientCount+ '">Ingredient Detail</a>'
     name = "recipe_link" 
     
     //console.log("Ingredients are", curr, document.getElementById("recipe_link").parentElement)
-    ingredientSelect.innerHTML = htmlContent
-    ingredientFields.appendChild(ingredientSelect)
+
+    
     
     //Gets the child node containing the Recipe dropdown ID
     temp_dropdown = document.getElementsByClassName(className)[0].childNodes[4]
+    
     // Changes the child node ID to match the class ID
-    temp_dropdown.id = "ingredient"+ ingredientCount
-    console.log(document.getElementsByClassName(className)[0].childNodes[4])
+    temp_dropdown.id = "ingredients"+ ingredientCount
+    
+    // Adds the getValX() to the code
+    document.getElementById("ingredients"+ ingredientCount).setAttribute('onchange','getVal('+ingredientCount+');');
+    console.log(document.getElementById("ingredient"+ ingredientCount))
    // Adds event listener for the newly created row
    start("ingredient"+ ingredientCount)
+
     }
 // Event Listener for the dropdown menus
 function start(num){
